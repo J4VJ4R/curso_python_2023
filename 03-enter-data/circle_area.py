be98990693d3a96abce04d02590b5796becaae9c
Javi <ingenierodesoftware73@gmail.com>
2023-08-17 12:50:51 -0500

    circle area and converter time

---
+++ 03-enter-data/circle_area.py
@@ -0,0 +1,12 @@
+print("="*25)
+print("--- CIRCLE AREA ---")
+print("="*25)
+area = 0
+pi = 3.14
+ratio = 0
+
+ratio = float(input("Enter your ratio: "))
+
+area = pi * ratio**2
+
+print("The area of the circle is: ", area)
---
+++ 03-enter-data/converter_time.py
@@ -0,0 +1,9 @@
+seconds = input("Enter time on seconds: ")
+seconds = int(seconds)
+minutes = seconds / 60
+hours = minutes / 60
+round_hours = round(hours, 2)
+
+print("Seconds: ", seconds)
+print("Minutes: ", minutes)
+print("Hours: ", round_hours)
