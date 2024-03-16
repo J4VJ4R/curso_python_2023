d2fae26774e226f57b922efcc05c2204537883a6
Javier <ingenierodesoftware73@gmail.com>
2024-03-16 17:53:11 -0500

    Add two number into console

---
+++ 02-variables-operators/add.py
@@ -0,0 +1,10 @@
+print('Suma de dos número enteros')
+
+#Entrada de datos
+a = input('Ingrese primer npúmero: ')
+b = input('Ingrese segundo número: ')
+
+#operacion
+suma = int(a) + int(b)
+
+print('la suma es: ', suma)
