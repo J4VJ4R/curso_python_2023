3e75c202d2b1053ad9f464ae256e4f6c33ef19bf
Javier <ingenierodesoftware73@gmail.com>
2024-03-22 18:20:13 -0500

    Is vowel

--- 02-variables-operators/practice2.py
+++ 02-variables-operators/practice2.py
@@ -1,5 +1,6 @@
 # Calculate sale price
 
+print('----- STORE COFFEE -----')
 item1 = input('Type price 1: ')
 item2 = input('Type price 2: ')
 
@@ -9,7 +10,8 @@ item2 = float(item2)
 price = item1 + item2
 taxes = price * 0.18
 totalprice = price + taxes
-
+print('='*30)
 print('Price without taxes: ', price)
 print('Taxes: ', taxes)
 print('Price with taxes: ', totalprice)
+print('='*30)
