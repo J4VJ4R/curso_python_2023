01eeaf623d385ffd50f5bd36546b134d46a9c6ec
Javier <ingenierodesoftware73@gmail.com>
2024-03-22 19:42:08 -0500

    for and while

---
+++ structures-control/for.py
@@ -0,0 +1,7 @@
+fruits = ['Apple', 'Pinaple', 'Orange']
+for fruit in fruits:
+  print(fruit)
+
+print('='*30)
+for n in range(10):
+  print(n)
---
+++ structures-control/while.py
@@ -0,0 +1,10 @@
+num1 = 5
+start = 0
+add = 0
+while start < num1:
+  add += start
+  start+=1
+  print(start)
+  
+print('='*30)
+print(add)
