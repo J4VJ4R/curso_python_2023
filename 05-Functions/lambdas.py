8175527c7d9ac3a240086d7aacd032c62c75cf9a
Javier <ingenierodesoftware73@gmail.com>
2024-04-06 06:44:47 -0500

    Lamba functions

---
+++ 05-Functions/lambdas.py
@@ -0,0 +1,10 @@
+sumar = lambda a,b:a+b
+doblar = lambda n: n * 2
+par = lambda n:n%2 == 0
+impar = lambda n:n%2 != 0
+inverse = lambda string: string[::-1]
+print(sumar(10, 20))
+print(doblar(2))
+print(par(1))
+print(impar(11))
+print(inverse('hello'))
