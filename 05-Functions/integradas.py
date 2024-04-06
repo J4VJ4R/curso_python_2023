7cacd92d623d49fb8b0690682c5f982191f67054
Javier <ingenierodesoftware73@gmail.com>
2024-04-06 07:20:57 -0500

    Integradas 2

--- 05-Functions/integradas.py
+++ 05-Functions/integradas.py
@@ -9,3 +9,32 @@ print(num2)
 num3 = float(40)
 print(num3)
 print(eval('2 + 5'))
+foo = 'foo foo'
+print((foo.upper()))
+foolower = 'FOO LOWER'
+print(foolower.lower())
+print(foo.capitalize())
+print(foolower.title())
+print(foo.count('o'))
+word = 'Python'
+word = word.replace('P', 'S')
+print(word)
+word2 = 'P y t h o n'
+word2 = word2.replace(' ', '')
+print(word2)
+word3 = ' Hello word  '
+print(word3.strip())
+word4 = '---Hello word---'
+print(word4.strip('-'))
+word5 = 'Hello Word'
+print(word5.split())
+word6 = 'Hello,world'
+print(word6.split(','))
+word7 = 'hello'
+print(word7.islower())
+word8 = 'HELLO'
+print(word8.isupper())
+word9 = 'Hello sord'
+print(word9.istitle())
+word10 = '     '
+print(word10.isspace())
