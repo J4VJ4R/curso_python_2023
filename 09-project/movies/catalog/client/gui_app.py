3cd3996027d72d08b6bd74c2b0ce76358fa33ba7
Javier <ingenierodesoftware73@gmail.com>
2024-04-22 04:52:27 -0500

    main_menu and bar_menu

--- 09-project/movies/catalog/client/gui_app.py
+++ 09-project/movies/catalog/client/gui_app.py
@@ -1,5 +1,22 @@
 import tkinter as tk
 
+def menu_bar(root):
+  bar_menu = tk.Menu(root)
+  root.config(menu = bar_menu, width=300, height = 300)
+
+  main_menu = tk.Menu(bar_menu, tearoff=0)
+  bar_menu.add_cascade(label='Home', menu = main_menu)
+  main_menu.add_command(label='Create register on DB')
+  main_menu.add_command(label='Delete register on DB')
+  main_menu.add_command(label='Exit', command=root.destroy)
+
+  query_data = tk.Menu(bar_menu, tearoff=0)
+  bar_menu.add_cascade(label='Query Data', menu=query_data)
+  query_data.add_command(label='Test1')
+  
+  bar_menu.add_cascade(label='Config')
+  bar_menu.add_cascade(label='Help')
+
 class Frame(tk.Frame):
   def __init__(self, root = None):
     super().__init__(root, width=480, height=320 )
--- 09-project/movies/catalog/movies.py
+++ 09-project/movies/catalog/movies.py
@@ -1,11 +1,11 @@
 import tkinter as tk
-from client.gui_app import Frame
+from client.gui_app import Frame, menu_bar
 def main():
   root = tk.Tk()
   root.title('Movies Catalog')
   # root.iconbitmap('09-project/movies/catalog/img/mov2.png')
   root.resizable(0,0)
-
+  menu_bar(root)
   app = Frame(root = root)
   app.mainloop()
 
