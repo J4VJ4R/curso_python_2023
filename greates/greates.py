28d40cd11a17c91240909453e751f783684f35d6
JavierJ <ingenierodesoftware73@gmail.com>
2024-07-31 14:53:44 -0500

    Documentting functions with peps

--- greates/greates.py
+++ greates/greates.py
@@ -1,6 +1,11 @@
 import random
 from typing import List, Dict
 def greats()->str:
+  """
+  Return a random great with name givving
+  Return:
+    str: Random greates with correct format
+  """
   greats = [
     "Hi {} is good see you egein",
     "Hi {} is perfect, you again!!",
@@ -9,6 +14,13 @@ def greats()->str:
   return random.choice(greats)
 
 def hi(name:str)->str:
+  """
+  Generete a personalized greate
+  Args:
+    name (str): Name of the user
+  Return:
+    Personalized greate (str)
+  """
   if name == "":
     print("The name is empty, try again")
   great = greats().format(name)
@@ -16,6 +28,13 @@ def hi(name:str)->str:
 
 
 def holas(names:List)->Dict:
+  """
+  Generete a personalized greate list
+  Args:
+    names (list[str]): Names of the users
+  Return:
+    Dict[str, str]: Dictionary with the greates with key and value
+  """
   greatslist = {}
   for name in names:
     greatsnew = hi(name)
