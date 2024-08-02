0d42a1ec2f32d7c75ac91fd12e634b71c7d67e8e
JavierJ <ingenierodesoftware73@gmail.com>
2024-08-02 15:27:22 -0500

    Polimorphism

--- 11-register-pets/pets.py
+++ 11-register-pets/pets.py
@@ -10,9 +10,23 @@ class Pet(Animal):
   def __init__(self, type, age, name) -> None:
     super().__init__(type, age)
     self.name = name
-  
-  def info(self):
-    print(f"The {self.type} is {self.age} her name is {self.name}")
 
-pet1 = Pet("Dog", "1 Year", "Moon")
-pet1.info()
+  def __str__(self) -> str:
+    return f"Animal[The {self.type} is {self.age} her name is {self.name}]"
+  
+  def speak(self):
+    pass
+
+class Dog(Pet):
+  def speak(self):
+    return "Woof!"
+  
+class Cat(Pet):
+  def speak(self):
+    return "Miau"
+  
+d = Dog("Dog", "1 year", "Waldo")
+c = Cat("Dog", "2 year", "Ana")
+
+print(d.speak())
+print(c.speak())
