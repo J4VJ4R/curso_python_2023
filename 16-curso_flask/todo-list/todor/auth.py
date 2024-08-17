a87fea0570dbac73c4a6b342c8393837b04bedf7
JavierJ <ingenierodesoftware73@gmail.com>
2024-08-17 14:04:30 -0500

    session

--- 16-curso_flask/todo-list/todor/auth.py
+++ 16-curso_flask/todo-list/todor/auth.py
@@ -1,5 +1,5 @@
 from flask import (
-  Blueprint, render_template, request, url_for, redirect, flash
+  Blueprint, render_template, request, url_for, redirect, flash, session, g
   )
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
@@ -22,9 +22,29 @@ def register():
       db.session.commit()
       return redirect(url_for('auth.login'))
     else:
-      error = f'El usuario {user_name} ya está registrado'
+      error = f'El usuario {username} ya está registrado'
+    flash(error)
   return render_template('auth/register.html')
 
-@bp.route('/login')
+@bp.route('/login', methods = ('GET', 'POST'))
 def login():
+  if request.method == 'POST':
+    username = request.form['username']
+    password = request.form['password']
+
+    #manejo de errores
+    error = None
+    #validar datos
+    user = User.query.filter_by(username = username).first()
+    if user == None:
+      error = "El nombre de usuario es incorrecto"
+    elif not check_password_hash(user.password, password):
+      error = "Contraseña incorrecta"
+    #iniciar sesión
+    if error == None:
+      session.clear()
+      session['user_id'] = user.id
+      return redirect(url_for('todo.index'))
+    
+    flash(error)
   return render_template('auth/login.html')
