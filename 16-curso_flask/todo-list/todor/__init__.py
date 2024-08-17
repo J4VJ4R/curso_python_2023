e527e5e05137fa0d9db0bca6dd9c9400d4d84362
JavierJ <ingenierodesoftware73@gmail.com>
2024-08-17 08:05:50 -0500

    Database conection and registers

--- 16-curso_flask/todo-list/todor/__init__.py
+++ 16-curso_flask/todo-list/todor/__init__.py
@@ -1,18 +1,28 @@
 from flask import Flask, render_template
-from . import todo
-from . import auth
+from flask_sqlalchemy import SQLAlchemy
+
+#Crear extensión
+db = SQLAlchemy()
 
 def create_app():
   app = Flask(__name__)
   #configuración del proyecto
   app.config.from_mapping(
     DEBUG = True,
-    SECRET_KEY = 'DEV'
+    SECRET_KEY = 'DEV',
+    SQLALCHEMY_DATABASE_URI = "sqlite:///todolist.db"
   )
+  #Inicio de la conexión en db
+  db.init_app(app)
   #Registrar bluprint
+  from . import todo
   app.register_blueprint(todo.bp)
+  from . import auth
   app.register_blueprint(auth.bp)
   @app.route('/')
   def index():
     return render_template('index.html')
+  #migración de modelos
+  with app.app_context():
+    db.create_all()
   return app
