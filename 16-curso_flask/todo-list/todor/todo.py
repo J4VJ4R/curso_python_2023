2036c1e36190447c6af64652f691d7b5a75133c6
JavierJ <ingenierodesoftware73@gmail.com>
2024-08-17 14:59:15 -0500

    cerrar sesión

--- 16-curso_flask/todo-list/todor/todo.py
+++ 16-curso_flask/todo-list/todor/todo.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('todo', __name__, url_prefix='/todo')
 
 @bp.route('/list')
 def index():
-  return "Lista de tareas"
+  return render_template('todo/index.html')
 
 @bp.route('/create')
 def create():
